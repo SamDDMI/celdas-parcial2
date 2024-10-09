b74d14db351fd15ff698245b91e7d794aac6aa6b
Sam <68865514+SamDDMI@users.noreply.github.com>
2024-10-09 10:34:32 -0600

    empezamos a juagr con la api de json

--- parcial2.0/controladores_de_pantallas/vista_celda/ControladorPantallaDeColeccion.swift
+++ parcial2.0/controladores_de_pantallas/vista_celda/ControladorPantallaDeColeccion.swift
@@ -10,45 +10,57 @@ import UIKit
 private let identificador_de_celda = "celda_pantalla_prin"
 
 class ControladorPantallaDeColeccion: UICollectionViewController {
-
+    
+    private var lista_de_publicaciones: [Post] = []
+    private let url_de_publicaciones = "https://jsonplaceholder.typicode.com/posts"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-     //   self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: identificador_de_celda)
-
-        // Do any additional setup after loading the view.
+        
+        let ubicacion = URL(string: url_de_publicaciones)!
+        URLSession.shared.dataTask(with: ubicacion){(datos, respuesta, error) in do {
+            
+            if let publicaciones_recibidas = datos{
+                let prueba_de_interpretacion_de_datos = try JSONDecoder().decode([Post].self, from: publicaciones_recibidas)
+                DispatchQueue.main.async{self.lista_de_publicaciones = prueba_de_interpretacion_de_datos}
+            }
+            else{
+                print("no se recibieron datos")
+            }
+            
+        } catch {print("error")
+        }
+        }.resume()
+        
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return 5 * 6
     }
-//funcion para identificar y crear cada una de las celdas creadas en el crontoller
+    //funcion para identificar y crear cada una de las celdas creadas en el crontoller
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celda: VistaDeCelda = collectionView.dequeueReusableCell(withReuseIdentifier: identificador_de_celda, for: indexPath) as! VistaDeCelda
-    
+        
         // Configure the cell
         
         
@@ -57,41 +69,41 @@ class ControladorPantallaDeColeccion: UICollectionViewController {
         celda.backgroundColor = .purple
         return celda
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
         print("se seleccionó la celda \(indexPath)")
     }
     
     
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
-
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+     
+     }
+     */
+    
 }
---
+++ viewmodel/post.swift
@@ -0,0 +1,20 @@
+//
+//  post.swift
+//  parcial2.0
+//
+//  Created by alumno on 09/10/24.
+//
+
+struct Post: Codable{
+    
+    //decodable
+    //encodable
+    //codable
+    //aqui jalaremos los datos de la api
+    var id: Int
+    var userID: Int
+    var title: String
+    var body: String
+    
+    
+}
